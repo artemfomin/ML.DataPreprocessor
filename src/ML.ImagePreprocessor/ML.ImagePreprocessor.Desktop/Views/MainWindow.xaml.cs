36dd60c0c7c17aa9487be72f8c70163a0a1d6bb2
Геннадий Тертычный <gonzyck@gmail.com>
2021-01-15 01:54:50 +0300

    Начало формирование структуры проекта

---
+++ src/ML.ImagePreprocessor/ML.ImagePreprocessor.Desktop/Views/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ML.ImagePreprocessor.Desktop
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
