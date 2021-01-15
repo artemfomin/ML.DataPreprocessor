6f0c4f7c6f385c7e82f4e02804e261dce2e0af4a
Геннадий Тертычный <gonzyck@gmail.com>
2021-01-15 03:14:26 +0300

    Определил ресурсы на увроне приложения, добавил вм

---
+++ src/ML.ImagePreprocessor/ML.ImagePreprocessor.Desktop/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using ML.ImagePreprocessor.Desktop.ViewModels.Base;
+using System.Windows.Input;
+
+namespace ML.ImagePreprocessor.Desktop.ViewModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        public string Title => "Finder";
+
+        public ICommand CloseApp => new CloseWindowCommand();
+    }
+}
---
+++ src/ML.ImagePreprocessor/ML.ImagePreprocessor.Desktop/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,9 @@
+﻿using ML.ImagePreprocessor.Desktop.ViewModels.Base;
+
+namespace ML.ImagePreprocessor.Desktop.ViewModels
+{
+    internal class ViewModelLocator
+    {
+        public MainWindowViewModel MainWindowModel => App.Host.Services.GetRequiredService<MainWindowViewModel>();
+    }
+}
