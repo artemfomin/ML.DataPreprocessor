6566ce20a7fba6176fdcd6bc02c6b1cff34d6db4
Геннадий Тертычный <gonzyck@gmail.com>
2021-01-15 03:20:43 +0300

    Добавил методы арсширения регистрации сервисов, добавил базовый класс команды и реализовал его.

---
+++ src/ML.ImagePreprocessor/ML.ImagePreprocessor.Desktop/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,21 @@
+﻿using ML.ImagePreprocessor.Desktop.Infrastructure.Commands.Base;
+using System;
+
+namespace ML.ImagePreprocessor.Desktop.Infrastructure.Commands
+{
+    internal class LambdaCommand<T> : Command
+    {
+        internal Action<T> _Execute;
+        internal Func<T, bool> _CanExecute;
+
+        public LambdaCommand(Action<T> Execute, Func<T, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke((T)parameter) ?? true;
+
+        public override void Execute(object parameter) => _Execute((T)parameter);
+    }
+}
