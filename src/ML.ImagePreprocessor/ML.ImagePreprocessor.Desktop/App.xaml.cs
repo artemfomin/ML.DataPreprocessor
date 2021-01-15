4428673c95b011e877a82c0c03700b337e4cd028
Геннадий Тертычный <gonzyck@gmail.com>
2021-01-15 03:24:44 +0300

    Сконфигурировал хост, определил IoC

--- src/ML.ImagePreprocessor/ML.ImagePreprocessor.Desktop/App.xaml.cs
+++ src/ML.ImagePreprocessor/ML.ImagePreprocessor.Desktop/App.xaml.cs
@@ -1,17 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ML.ImagePreprocessor.Desktop.Infrastructure.Services;
+using ML.ImagePreprocessor.Desktop.ViewModels;
+using System;
+using System.IO;
+using System.Runtime.CompilerServices;
 using System.Windows;
 
 namespace ML.ImagePreprocessor.Desktop
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public static IHost __Host;
+        public static IHost Host => __Host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+        public static bool IsDesignMode { get; private set; } = true;
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            IsDesignMode = false;
+
+            var host = Host;
+            base.OnStartup(e);
+
+            await host.StartAsync().ConfigureAwait(false);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            var host = Host;
+            await host.StopAsync().ConfigureAwait(false);
+            host.Dispose();
+            __Host = null;
+        }
+
+        public static void ConfigureServies(HostBuilderContext host, IServiceCollection services) => services
+            .ViewModelRegistrate()
+            .ServicesRegistrator();
+
+        public static string CurrentDirectory => IsDesignMode ? Path.GetDirectoryName(GetSourceCodePath()) : Environment.CurrentDirectory;
+
+        private static string GetSourceCodePath([CallerFilePath] string Path = null) => Path;
     }
 }
---
+++ src/ML.ImagePreprocessor/ML.ImagePreprocessor.Desktop/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace ML.ImagePreprocessor.Desktop
+{
+    internal class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] Args) =>
+           Host.CreateDefaultBuilder()
+            .UseContentRoot(App.CurrentDirectory)
+            .ConfigureAppConfiguration((host, cfg) => cfg
+                .SetBasePath(App.CurrentDirectory)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true))
+            .ConfigureServices(App.ConfigureServies);
+    }
+}
